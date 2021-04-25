a4652541033ac205f99c50ac402a59e3b0e6cd82
bdcry  <tim040404@mail.ru>
2021-04-25 21:57:53 +0300

    commit 1

--- api_rc/models.py
+++ api_rc/models.py
@@ -1,3 +1,97 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models import Avg
+from .validators import score_validator
 
-# Create your models here.
+
+class Roles(models.TextChoices):
+    USER = 'user', 'Пользователь'
+    MODERATOR = 'moderator', 'Модератор'
+    ADMIN = 'admin', 'Администратор'
+
+class User(AbstractUser):
+    bio = models.TextField('О себе', blank=True, null=True, )
+    email = models.EmailField('Email', unique=True)
+    role = models.CharField('Роль', max_length=10, choices=Roles.choices,
+                            default=Roles.USER)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ('username',)
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+        ordering = ('username',)
+
+
+class Review(models.Model):
+    score = models.PositiveSmallIntegerField(
+        'Оценка',
+        choices=[(r, r) for r in range(1, 11)],
+        validators=[score_validator],
+    )
+    text = models.TextField('Текст', blank=True, null=True)
+    pub_date = models.DateTimeField(
+        'Дата отзыва',
+        auto_now_add=True,
+        db_index=True,
+    )
+    author = models.ForeignKey(
+        User,
+        verbose_name='Автор',
+        on_delete=models.CASCADE,
+    )
+    title = models.ForeignKey(
+        Title,
+        related_name='reviews',
+        verbose_name='Произведение',
+        on_delete=models.CASCADE,
+    )
+
+    class Meta:
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
+        ordering = ('-pub_date',)
+
+    def __str__(self):
+        return self.text
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        self.score_avg = Review.objects.filter(title_id=self.title).aggregate(
+            Avg('score')
+        )
+        self.title.rating = self.score_avg['score__avg']
+        self.title.save()
+
+
+class Comment(models.Model):
+    text = models.TextField('Комментарий', blank=True, null=True)
+
+    author = models.ForeignKey(
+        User,
+        verbose_name='Автор',
+        on_delete=models.CASCADE,
+    )
+    pub_date = models.DateTimeField(
+        'Дата',
+        db_index=True,
+        auto_now_add=True,
+    )
+
+    review = models.ForeignKey(
+        Review,
+        related_name='comments',
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+        verbose_name='Комментарий'
+    )
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+        ordering = ('-pub_date',)
+
+    def __str__(self):
+        return self.text
--- api_rc/views.py
+++ api_rc/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.generics import get_object_or_404
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .models import Review
+from .permissions import ReviewCommentPermissions
+from .serializers import CommentSerializer, ReviewSerializer
 
-# Create your views here.
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    serializer_class = ReviewSerializer
+    pagination_class = PageNumberPagination
+    permission_classes = [ReviewCommentPermissions,
+                          IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        title = get_object_or_404(Title, pk=self.kwargs.get('title_id'))
+        serializer.save(author=self.request.user, title=title)
+
+    def get_queryset(self):
+        title = get_object_or_404(Title, pk=self.kwargs.get('title_id'))
+        return title.reviews.all()
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    pagination_class = PageNumberPagination
+    permission_classes = [ReviewCommentPermissions,
+                          IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        review = get_object_or_404(Review, pk=self.kwargs.get('review_id'))
+        serializer.save(author=self.request.user, review=review)
+
+    def get_queryset(self):
+        review = get_object_or_404(Review, pk=self.kwargs.get('review_id'))
+        return review.comments.all()
