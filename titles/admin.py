bc247f8c3770b1046cd26c5565b477f319b32b91
Никита <freegond@yandex.ru>
2021-04-25 22:52:42 +0400

    first "titles" commit

---
+++ titles/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+
+from .models import Category, Genre, Title
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'slug')
+    search_fields = ('name',)
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'slug')
+    search_fields = ('name',)
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Title)
+class TitleAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'year', 'description', 'category')
+    search_fields = ('name',)
+    empty_value_display = '-пусто-'
