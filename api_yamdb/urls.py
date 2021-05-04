844a7de68009d5f5782b9fd7f85acd987f836ecf
bdcry  <tim040404@mail.ru>
2021-05-05 00:10:49 +0300

    Final

--- api_yamdb/urls.py
+++ api_yamdb/urls.py
@@ -12,16 +12,6 @@ urlpatterns = [
         TemplateView.as_view(template_name='redoc.html'),
         name='redoc'
     ),
-    path(
-        'api/v1/token/',
-        TokenObtainPairView.as_view(),
-        name='token_obtain_pair'
-    ),
-    path(
-        'api/v1/token/refresh/',
-        TokenRefreshView.as_view(),
-        name='token_refresh'
-    ),
     path('api/', include('api.urls')),
     path('api/', include('titles.urls')),
 ]
