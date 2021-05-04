110a3387c9b9761d3a9ae920a71c517f9eb94214
bdcry  <tim040404@mail.ru>
2021-05-05 00:12:48 +0300

    Final

--- api_yamdb/urls.py
+++ api_yamdb/urls.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
-from rest_framework_simplejwt.views import (TokenObtainPairView,
-                                            TokenRefreshView)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
